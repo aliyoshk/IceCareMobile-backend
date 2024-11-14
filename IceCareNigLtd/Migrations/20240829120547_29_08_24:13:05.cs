a3d17a21ce10c83badc8d6d3f5fe80bc44e59198
Aliyu Khalifa <aliyu.khalifa@wemabank.com>
2024-11-14 16:10:44 +0100

    no message

---
+++ IceCareNigLtd/Migrations/20240829120547_29_08_24:13:05.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IceCareNigLtd.Migrations
+{
+    public partial class _29_08_241305 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalDollarAmount",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Channel",
+                table: "Suppliers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Channel",
+                table: "Suppliers");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalDollarAmount",
+                table: "Customers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
