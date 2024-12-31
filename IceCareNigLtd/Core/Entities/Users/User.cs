d3a4c98e117a7a2d2dfa0266c62aff3f62418a62
Aliyu Khalifa <aliyu.khalifa@wemabank.com>
2024-12-31 15:57:33 +0100

    no message

---
+++ IceCareNigLtd/Core/Entities/Users/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace IceCareNigLtd.Core.Entities.Users
+{
+	public class User
+	{
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime Date { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal BalanceNaira { get; set; }
+        public decimal BalanceDollar { get; set; }
+    }
+}
+
